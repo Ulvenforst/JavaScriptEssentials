1eb7b6e800ad05b36185e362c0f61ebbdfe04e99
Juan Camilo Narváez <ulvenforst@gmail.com>
2023-02-09 19:25:11 -0500

    Exercise 03_07 has been done

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,29 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const cellphone = {
+  owner: "Juan Camilo Narváez",
+  number: "3182249713",
+  operator: "Tigo",
+  model: "iPhone SE 2022",
+  space: 128, // GB
+};
+
+const laptop = {
+  owner: "Juan Camilo Narváez",
+  model: "Rog Zephyrus G14",
+  space: 1000, // GB
+  ram: 24,
+  size: 14 // Inches
+};
+
+const suitcase = {
+  primaryPocket: {
+    bigPocket: laptop,
+    smallPocket: cellphone
+  },
+  secundaryPocket: {
+    isEmpty: true
+  }
+};
